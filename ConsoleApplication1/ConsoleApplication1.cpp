620396b5578c2ee48bc41db463cf1bfa10d5a692
Админ <@DESKTOP-RQEKI85>
2024-04-15 18:56:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+    int main() {
+        setlocale(LC_ALL, "ru");
+        vector<int> vec;
+        int a;
+
+        cout << "Введите сколько чисел хотите ввести: ";
+        cin >> a;
+        for (int i = 0; i < a; i++) {
+            int b;
+            cin >> b;
+            vec.push_back(b);
+        }
+        sort(vec.begin(), vec.end());
+        for (auto el : vec) {
+            cout << el<<" ";
+        }
+ 
+        return 0;
+    }
+
